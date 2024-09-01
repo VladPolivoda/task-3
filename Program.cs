2257a737f010eea50e61e46767c55cb0b8601694
VladPolivoda <158571784+VladPolivoda@users.noreply.github.com>
2024-09-01 15:24:57 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть довжини сторін трикутника a, b, c: ");
+        string[] input = Console.ReadLine().Split(' ');
+        double a = Convert.ToDouble(input[0]);
+        double b = Convert.ToDouble(input[1]);
+        double c = Convert.ToDouble(input[2]);
+
+        if ((a + b > c) && (a + c > b) && (b + c > a))
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Трикутник з сторонами {a}, {b}, {c} існує.");
+
+            if (a == b || b == c || a == c)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine($"Трикутник з сторонами {a}, {b}, {c} є рівнобедреним.");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Трикутник з сторонами {a}, {b}, {c} не є рівнобедреним.");
+            }
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Трикутник з сторонами {a}, {b}, {c} не існує.");
+        }
+
+        Console.ResetColor();
+    }
+}
